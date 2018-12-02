96bdca160a8368f7bedf2cca18a3753cc5ca1b56
Drew Knab <drewknab@gmail.com>
2018-12-02 10:03:17 -0500

    Day one catchup complete. All JS all the time.

---
+++ 20181201_Chronal_Calibration.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync('input/20181201_Chronal_Calibration.txt', 'utf-8');
+
+const chrons = input.trim()
+                    .split('\n')
+                    .map(x => parseInt(x))
+
+// Part 1
+console.log(chrons.reduce((sum, current) => sum + current));
+
+// Part 2
+const sum_list = new Set([0]);
+let sum = 0;
+let result = 0;
+let length = chrons.length;
+
+for(let i = 0; ; i = ((i+1) % length)) {
+  sum += chrons[i];
+        
+  if (sum_list.has(sum)) {
+    console.log(sum);
+    break;
+  }
+
+  sum_list.add(sum);
+}
