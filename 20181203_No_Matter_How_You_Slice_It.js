104392adda97b131f57e56543e0dfa5072e610cf
Drew Knab <drewknab@gmail.com>
2018-12-03 09:50:34 -0500

    fix author prep day 3

---
+++ 20181203_No_Matter_How_You_Slice_It.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync('input/20181202_Inventory_Management_System.txt', 'utf-8');
+const ids = input.trim()
+                 .split('\n')
+
+// Code
