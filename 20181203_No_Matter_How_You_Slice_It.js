a9bb18ba3ac3677688a5834d805196ba8302af43
Drew Knab <drewknab@gmail.com>
2018-12-03 14:12:36 -0500

    do again but better

--- 20181203_No_Matter_How_You_Slice_It.js
+++ 20181203_No_Matter_How_You_Slice_It.js
@@ -4,17 +4,43 @@ const rects = input.trim()
                  .split('\n')
                  .map(data => {
                     return {
-                      'id': data.split(' ')[0].replace(/#/g,''),
-                      'ords': {
-                        'left': data.split(' ')[2].replace(/:/g,'').split(',')[0],
-                        'top': data.split(' ')[2].replace(/:/g,'').split(',')[1]
+                      'id': parseInt(data.split(' ')[0].replace(/#/g,'')),
+                      'topLeft': {
+                        'x': parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[0]) + 1,
+                        'y': parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[1]) + 1
+                      },
+                      'bottomRight': {
+                        'x': (parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[0]) + 1) + parseInt(data.split(' ')[3].split('x')[0]),
+                        'y': (parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[1]) + 1) + parseInt(data.split(' ')[3].split('x')[1]),
                       },
                       'size': {
-                        'width': data.split(' ')[3].split('x')[0],
-                        'height': data.split(' ')[3].split('x')[1],
+                        'w': parseInt(data.split(' ')[3].split('x')[0]),
+                        'h': parseInt(data.split(' ')[3].split('x')[1]),
                       }
                     }
                  })
-//console.log(rects)
+console.log(rects.length)
 // Code
+const length = rects.length
+const overlapRects = new Set([0])
+const checkOverlap = (rect1, rect2) => {
+  if (rect1.topLeft.x > rect2.bottomRight.x || rect2.topLeft.x > rect1.bottomRight.x)
+    return false
 
+  if (rect1.topLeft.y > rect2.bottomRight.y || rect2.topLeft.y > rect1.bottomRight.y)
+    return false
+
+  return true
+}
+for (let i = 0; i < length; i++) {
+  for (let j = i + 1; j < length; j++) {
+    let rect_i = rects[i]
+    let rect_j = rects[j]
+
+    if (checkOverlap(rect_i, rect_j)) {
+      overlapRects.add(rect_i)
+      overlapRects.add(rect_j)
+    }
+  }
+}
+console.log(overlapRects.size);
