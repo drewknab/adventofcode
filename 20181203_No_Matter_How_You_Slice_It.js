b6867cbe610a3d3a2d9ce1c82fda27ed7764f01d
Drew Knab <drewknab@gmail.com>
2018-12-03 13:05:54 -0500

    assemble rectangle objects

--- 20181203_No_Matter_How_You_Slice_It.js
+++ 20181203_No_Matter_How_You_Slice_It.js
@@ -1,6 +1,20 @@
 const fs = require('fs');
-const input = fs.readFileSync('input/20181202_Inventory_Management_System.txt', 'utf-8');
-const ids = input.trim()
+const input = fs.readFileSync('input/20181203_No_Matter_How_You_Slice_It.txt', 'utf-8');
+const rects = input.trim()
                  .split('\n')
-
+                 .map(data => {
+                    return {
+                      'id': data.split(' ')[0].replace(/#/g,''),
+                      'ords': {
+                        'left': data.split(' ')[2].replace(/:/g,'').split(',')[0],
+                        'top': data.split(' ')[2].replace(/:/g,'').split(',')[1]
+                      },
+                      'size': {
+                        'width': data.split(' ')[3].split('x')[0],
+                        'height': data.split(' ')[3].split('x')[1],
+                      }
+                    }
+                 })
+//console.log(rects)
 // Code
+
