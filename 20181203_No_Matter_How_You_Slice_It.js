97f3707c1e7fa726bcef1cc2272265eccddd2ad8
Drew Knab <drewknab@gmail.com>
2018-12-03 23:51:34 -0500

    finished day 3

--- 20181203_No_Matter_How_You_Slice_It.js
+++ 20181203_No_Matter_How_You_Slice_It.js
@@ -4,43 +4,54 @@ const rects = input.trim()
                  .split('\n')
                  .map(data => {
                     return {
-                      'id': parseInt(data.split(' ')[0].replace(/#/g,'')),
-                      'topLeft': {
-                        'x': parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[0]) + 1,
-                        'y': parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[1]) + 1
-                      },
-                      'bottomRight': {
-                        'x': (parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[0]) + 1) + parseInt(data.split(' ')[3].split('x')[0]),
-                        'y': (parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[1]) + 1) + parseInt(data.split(' ')[3].split('x')[1]),
-                      },
-                      'size': {
-                        'w': parseInt(data.split(' ')[3].split('x')[0]),
-                        'h': parseInt(data.split(' ')[3].split('x')[1]),
-                      }
+                      id: parseInt(data.split(' ')[0].replace(/#/g,'')),
+                      top: parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[1]),
+                      left: parseInt(data.split(' ')[2].replace(/:/g,'').split(',')[0]),
+                      width: parseInt(data.split(' ')[3].split('x')[0]),
+                      height: parseInt(data.split(' ')[3].split('x')[1]),
+                      fail: 0,
                     }
                  })
-console.log(rects.length)
-// Code
-const length = rects.length
-const overlapRects = new Set([0])
+
+// RIP Array.fill() doesn't play with multidimensional for loops?
+// const fabric = Array(1000).fill(Array(1000).fill(0))
+// I give up on clever, here is a dumb matrix.
+const fabric = Array
+  .from({length: 1000})
+  .map(() => Array
+    .from({length: 1000})
+    .map(() => 0)
+  )
+
+// Paint the dumb matrix
+rects.forEach((rect) => {
+  for (let y = 0; y < rect.height; y++) {
+    for (let x = 0; x < rect.width; x++) {
+        fabric[y + rect.top][x + rect.left] += 1
+    }
+  }
+})
+
+console.log(fabric.reduce((a, b) => a + b.filter(x => x >= 2).length, 0))
+
 const checkOverlap = (rect1, rect2) => {
-  if (rect1.topLeft.x > rect2.bottomRight.x || rect2.topLeft.x > rect1.bottomRight.x)
+  if (rect1.left > (rect2.left + rect2.width) || rect2.left > (rect1.left + rect1.width))
     return false
 
-  if (rect1.topLeft.y > rect2.bottomRight.y || rect2.topLeft.y > rect1.bottomRight.y)
+  if (rect1.top > (rect2.top + rect2.height) || rect2.top > (rect1.top + rect1.height))
     return false
 
   return true
 }
-for (let i = 0; i < length; i++) {
-  for (let j = i + 1; j < length; j++) {
-    let rect_i = rects[i]
-    let rect_j = rects[j]
 
-    if (checkOverlap(rect_i, rect_j)) {
-      overlapRects.add(rect_i)
-      overlapRects.add(rect_j)
+const length = rects.length;
+rects.forEach((rect, index, arr) => {
+  for (let i = index + 1; i < length; i++) {
+    if (checkOverlap(rect, arr[i])) {
+      rects[index].fail += 1
+      rects[i].fail += 1
     }
   }
-}
-console.log(overlapRects.size);
+})
+
+console.log(rects.filter(x => x.fail == 0)[0].id)
