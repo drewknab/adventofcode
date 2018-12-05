3067cf93e840d08182fa6d8d42156da2649b8b50
Drew Knab <drewknab@gmail.com>
2018-12-04 21:43:10 -0500

    ugh

--- 20181204_Repose_Record.js
+++ 20181204_Repose_Record.js
@@ -14,35 +14,80 @@ const status = input.trim()
                       return a.date > b.date ? 1 : -1
                     })
 
-let times = {}
-let minutes = {}
-let guardMinutes = {}
-let table = {}
+const returnMax = (obj) => reduceObject(Object.entries(obj), entry => entry[1])
+const reduceObject = (array, criteria) => array.reduce((a,b) => criteria(a) > criteria(b) ? a : b)
+
+// Break down guards
+let guardTime = {}
 let currentGuard = 0
 let sleepBegin = 0
-let start = 0
 status.forEach((record) => {
   let time = new Date(record.date).getTime();
-  let minute = record.date.match(/-?[0-9]+/g || []).map(Number)[4]
   if (record.text.startsWith('Guard')) {
-    currentGuard = record.text.match(/-?[0-9]+/g || []).map(Number)
-    table = (guardMinutes[currentGuard] || {})
-    guardMinutes[currentGuard] = table
+    [currentGuard] = record.text.match(/-?[0-9]+/g).map(Number)
   }
   if (record.text == 'wakes up') {
-    times[currentGuard] = (times[currentGuard] || 0) + (time - sleepBegin)
-    for (i = start; i !== minute; i = (i + 1) % 60) {
-      minutes[i] = (minutes[i] || 0) + 1
-      table[i] = (table[i] || 0) + 1
-    }
+    guardTime[currentGuard] = (guardTime[currentGuard] || 0) + (time - sleepBegin)
   }
   if (record.text == 'falls asleep') {
     sleepBegin = time
-    start = minute
   }
 })
-console.log(times)
-let sleepiest = Object.keys(times).reduce((a, b) => times[a] > times[b] ? a : b)
-let asleepest = Object.keys(minutes).reduce((a, b) => minutes[a] > minutes[b] ? a : b)
-let guardiest = Object.keys(guardMinutes[sleepiest]).reduce((a, b) => guardMinutes[a] > guardMinutes[b] ? a : b)
-//console.log(sleepiest * 42)
+
+// Since we have our guard, grab times
+let ourBoy = returnMax(guardTime)[0]
+let minutes = {}
+let startMinute = 0
+status.forEach((record) => {
+  let minute = record.date.match(/-?[0-9]+/g || []).map(Number)[4]
+  if (record.text.startsWith('Guard'))
+    [currentGuard] = record.text.match(/-?[0-9]+/g).map(Number)
+  
+  if (record.text == 'wakes up') {
+    if (currentGuard != ourBoy) {
+      return;
+    }
+
+    for (i = startMinute; i != minute; i = (i + 1) % 60)
+      minutes[i] = (minutes[i] || 0) + 1;
+  }
+
+  if (record.text == 'falls asleep') 
+    startMinute = minute
+})
+let ourTime = returnMax(minutes)[0]
+
+// Part 1
+console.log(ourTime * ourBoy)
+
+// We're looping again, by god
+let guardMinutes = {}
+let currentTable = {}
+startMinute = 0
+status.forEach((record) => {
+  let minute = record.date.match(/-?[0-9]+/g || []).map(Number)[4]
+  if (record.text.startsWith('Guard')) {
+    [currentGuard] = record.text.match(/-?[0-9]+/g).map(Number)
+    currentTable = (guardMinutes[currentGuard] || {})
+    guardMinutes[currentGuard] = currentTable
+  }
+  
+  if (record.text == 'wakes up') {
+    for (i = startMinute; i != minute; i = (i + 1) % 60)
+      currentTable[i] = (currentTable[i] || 0) + 1;
+  }
+
+  if (record.text == 'falls asleep') 
+    startMinute = minute
+})
+
+let finish = reduceObject(Object.entries(guardMinutes).map(([key, value]) => {
+  if (!Object.entries(value).length) { 
+    return [key, 0, 0];
+  }
+
+  return [key].concat(returnMax(value));
+}), a => a[2])
+
+// Part 2
+console.log(finish[0] * finish[1])
