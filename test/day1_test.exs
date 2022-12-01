307f01818bc9add7d12ba1f30eb629f73206a98c
Drew Knab <drewknab@gmail.com>
2022-12-01 00:47:51 -0500

    day one get

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,14 +2,27 @@ defmodule AdventOfCode.Day1Test do
   use ExUnit.Case
 
   test "test part 1" do
-    assert AdventOfCode.Day1.part1(test_input()) == ""
+    assert AdventOfCode.Day1.part1(test_input()) == 24000
   end
 
   test "test part 2" do
-    assert AdventOfCode.Day1.part2(test_input()) == ""
+    assert AdventOfCode.Day1.part2(test_input()) == 45000
   end
 
   defp test_input() do
-    ""
+    "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
   end
 end
